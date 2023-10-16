a6f72a13b330514e7bf5f5c3c20593f935a83c44
irina777-create <71390012+irina777-create@users.noreply.github.com>
2023-10-16 10:14:39 +0300

    создание js , задание 1

---
+++ .history/main_20231016100322.js
@@ -0,0 +1,7 @@
+const a='name';
+const b='FirstName';
+const c='age';
+const name=prompt("Введите ім'я",0);
+const FirstName=prompt("Введите фамилию",0);
+const age=prompt("Введите возраст",0);
+
---
+++ main.js
@@ -0,0 +1,8 @@
+const a='name';
+const b='FirstName';
+const c='age';
+const name=prompt("Введите ім'я",0);
+const FirstName=prompt("Введите фамилию",0);
+const age=prompt("Введите возраст",0);
+alert(name+FirstName+age);
+
